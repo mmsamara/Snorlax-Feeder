7e5264ae9bdd8ca5bd59dba76085f169f2959bf4
mmsamara <mmsamara@ncsu.edu>
2018-02-17 15:33:03 -0500

    Fixed Issues

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,106 @@
+var wins = 0;
+var losses = 0;
+var currentFullness = 0;
+var randomNumber;
+var cakeNumber;
+var lobsterNumber;
+var sharkNumber;
+var chickenNumber;
 
 $(document).ready(function() {
 
-	var wins = 0;
-	var losses = 0;
-	var currentFullness = 0;
 
-	// function initializeGame() {
-		
-	// 	$("#current-fullness, #goal-number").empty();
-	// };
+	function initializeGame() {
 
-	var randomNumber = Math.floor(Math.random() * 101) + 19;
-	var cakeNumber = Math.floor(Math.random() * 12) + 1;
-	var lobsterNumber = Math.floor(Math.random() * 12) + 1;
-	var sharkNumber = Math.floor(Math.random() * 12) + 1;
-	var chickenNumber = Math.floor(Math.random() * 12) + 1;
+		currentFullness = 0;
+		randomNumber = Math.floor(Math.random() * 101) + 19;
+		cakeNumber = Math.floor(Math.random() * 12) + 1;
+		lobsterNumber = Math.floor(Math.random() * 12) + 1;
+		sharkNumber = Math.floor(Math.random() * 12) + 1;
+		chickenNumber = Math.floor(Math.random() * 12) + 1;
 
-	$("#goal-number").text(randomNumber);
-	$("#current-fullness").text(currentFullness);
-	$("#wins-area").text(wins);
-	$("#losses-area").text(losses);
-
-	$("#cake").click(function() {
-		currentFullness += cakeNumber;
 		$("#goal-number").text(randomNumber);
 		$("#current-fullness").text(currentFullness);
 		$("#wins-area").text(wins);
 		$("#losses-area").text(losses);
+		
+	};
+
+	if (currentFullness === randomNumber) {
+		wins++;
+		$("#result-message").text("You Win!");
+		initializeGame();
+	}
+	else if (currentFullness > randomNumber) {
+		losses++;
+		$("#result-message").text("You Lose :(");
+		initializeGame();
+	}
+
+	$("#cake").click(function() {
+		currentFullness += cakeNumber;
+		$("#current-fullness").text(currentFullness);
 		if (currentFullness === randomNumber) {
 			wins++;
 			$("#result-message").text("You Win!");
+			initializeGame();
 		}
 		else if (currentFullness > randomNumber) {
 			losses++;
 			$("#result-message").text("You Lose :(");
-			//initializeGame();
+			initializeGame();
 		}
 	});
 
 	$("#lobster").click(function() {
 		currentFullness += lobsterNumber;
-		$("#goal-number").text(randomNumber);
 		$("#current-fullness").text(currentFullness);
-		$("#wins-area").text(wins);
-		$("#losses-area").text(losses);
 		if (currentFullness === randomNumber) {
 			wins++;
 			$("#result-message").text("You Win!");
+			initializeGame();
 		}
 		else if (currentFullness > randomNumber) {
 			losses++;
 			$("#result-message").text("You Lose :(");
-			//initializeGame();
+			initializeGame();
 		}
 	});
 
 	$("#shark").click(function() {
 		currentFullness += sharkNumber;
-		$("#goal-number").text(randomNumber);
 		$("#current-fullness").text(currentFullness);
-		$("#wins-area").text(wins);
-		$("#losses-area").text(losses);
 		if (currentFullness === randomNumber) {
 			wins++;
 			$("#result-message").text("You Win!");
+			initializeGame();
 		}
 		else if (currentFullness > randomNumber) {
 			losses++;
 			$("#result-message").text("You Lose :(");
-			//initializeGame();
+			initializeGame();
 		}
 	});
 
 	$("#chicken").click(function() {
 		currentFullness += chickenNumber;
-		$("#goal-number").text(randomNumber);
 		$("#current-fullness").text(currentFullness);
-		$("#wins-area").text(wins);
-		$("#losses-area").text(losses);
 		if (currentFullness === randomNumber) {
 			wins++;
 			$("#result-message").text("You Win!");
+			initializeGame();
 		}
 		else if (currentFullness > randomNumber) {
 			losses++;
 			$("#result-message").text("You Lose :(");
-			//initializeGame();
+			initializeGame();
 		}
 	});
 
-	$("#goal-number").text(randomNumber);
-		$("#current-fullness").text(currentFullness);
-		$("#wins-area").text(wins);
-		$("#losses-area").text(losses);
+	initializeGame();
+
+	console.log(cakeNumber);
+	console.log(sharkNumber);
+	console.log(lobsterNumber);
+	console.log(chickenNumber);
 });
